f5902efb1eebeaf8113e20ef77c8008633519e0f
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2023-06-07 09:41:19 +0200

    saved and commit again to make sure everything is included, because something was unsaved in the first commit

---
+++ js/coffemaker.js
@@ -0,0 +1,12 @@
+
+const coffemaker = {
+    brand: "moccamaster"
+    volume: 1.25
+    price: 2199
+    weight: 3.9
+    on: false
+    color: ["black", "white", "green", "blue", "grey", "red"] 
+    ignition: function () {
+        coffemaker.on = !coffemaker.on
+    }
+}
